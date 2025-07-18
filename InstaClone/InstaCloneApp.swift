b66b7cf27780b940b7181f1f0b10c10de56f0d74
Rümeysa Tokur <rtokur11@gmail.com>
2025-07-18 14:33:48 +0300

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Rumeysa Tokur on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
